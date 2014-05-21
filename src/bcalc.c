90121d6e5a2397de93cfbea63e718b301f2f1fde
Ben Wei <ben@staros.mobi>
2014-05-21 13:14:17 +0800

    missing bcalc.c and refactor tests

---
+++ src/bcalc.c
@@ -0,0 +1,61 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <setjmp.h>
+#include <stdlib.h>
+#include "parse.h"
+#include <stdarg.h>
+
+static jmp_buf onError;
+void error (const char * fmt, ...);
+
+#define BCALC_VERSION "0.1"
+#define BCALC_BUILD   "1000"
+#define BCALC_NAME    "bcalc"
+
+static inline void show_top_banner()
+{
+    printf("%s version %s.%s\n", BCALC_NAME, BCALC_VERSION, BCALC_BUILD);
+}
+
+static inline void calc_line_by_line_from_stdin(void)
+{
+    char buf [BUFSIZ] = {0};
+    while (gets(buf))
+    {
+        if (scan(buf)) {
+            void *e = sum();
+            if (token)
+                error("trash after sum");
+            process(e);
+            delete(e);
+        }
+    }
+}
+
+int main (void)
+{
+    volatile int errors = 0;
+
+    show_top_banner();
+
+    /* if there are error raised by error() called,
+     * programm will back to next line */
+    if (setjmp(onError))
+        ++errors;
+
+    calc_line_by_line_from_stdin();
+
+    return errors > 0;
+}
+
+void error (const char * fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap), putc('\n', stderr); va_end(ap);
+    longjmp(onError, 1);
+}
---
+++ src/tests.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+# tests cases written by ben6 2014-05
+
+target="$1"
+if [ -z "$target" ]; then
+target="bcalc"
+fi
+
+docalc() {
+    ret=`echo "${myexp}" | ./${target} | strings | grep "process result =" | sed -e "s/^.*=[ ]*\([0-9.]*\)$/\1/g"`
+    eval "$1=$ret"
+}
+
+docalc_from_file() {
+    ret=`cat "$1" | ./${target} | strings | grep "process result =" | sed -e "s/^.*=[ ]*\([0-9.]*\)$/\1/g"`
+    echo "$ret"
+}
+
+show_result ()
+{
+    ret="$1"
+    compare="$2"
+    exp="$3"
+    if [ "$ret" = "${compare}" ]; then
+        echo "OK, origin=$exp, result=$ret"
+    else
+        echo "failed, origin=$exp, result=$ret"
+    fi
+}
+
+check_multiple_result_from_file()
+{
+    counter=1
+    test_file="$1"
+    shift
+    docalc_from_file "$test_file" | while read r ; do
+        eval "expdisp=\"`cat $test_file | head -n $counter | tail -n 1`\""
+        show_result "$r" "$1" "$expdisp"
+        counter=$((counter + 1))
+        shift
+    done
+}
+
+check_single_result()
+{
+    docalc result
+    show_result "$result" "$1" "$myexp"
+}
+
+myexp="2 * 2 + 10 * 2 + 4 / 8 + 1"
+check_single_result "25.5"
+
+myexp="(2 * 2) + 10 * (2 + 4) / 8 + 1"
+check_single_result "12.5"
+
+myexp="(2 * -2) + 10 * (2 + 4) / 8 - 1"
+check_single_result "2.5"
+
+check_multiple_result_from_file "tests/file01.txt" "0.5" "20" "990"
